c8c4e7dfa3d20511820c5560f3dc88471489c020
Ivan Chernyshev <l3tu4iy@gmail.com>
2023-09-26 18:56:07 +0500

    mod1

---
+++ JavaSpring/src/main/java/spring_introduction/Dog.java
@@ -0,0 +1,23 @@
+package spring_introduction;
+
+public class Dog implements Pet {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Dog() {
+        System.out.println("Dog bean is created");
+    }
+
+    @Override
+    public void say() {
+        System.out.println("Bow-Wow");
+    }
+}
+
